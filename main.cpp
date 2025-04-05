3b33f0ab12313855d2ff084b782e152aa5128666
Ekki DF4OR <ekki@plicht.de>
2025-04-05 08:55:47 +0200

    Test commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // comment 2025-04-05
+
     // to be used by QSettings, determines the path where settings are stored
     a.setOrganizationName("ztl.space");
     a.setOrganizationDomain("ztl.space");
